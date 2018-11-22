a02dc6d5adc78eef900a827bb82116dcae876e89
siyu <hsy_ren@163.com>
2018-11-22 10:39:06 +0800

    delete a project

--- Appium_testcase/src/utils/InputPW.py
+++
@@ -1,9 +0,0 @@
-from src.utils.ConductButton import ConductButton
-from src.utils.constant import const
-
-class InputPW:
-    def inputPW(self):
-        ConductButton().clickButton(const.btn_one)
-        ConductButton().clickButton(const.btn_two)
-        ConductButton().clickButton(const.btn_three)
-        ConductButton().clickButton(const.btn_four)
